b4d66265ab36892d1947fb00225265f2336185ac
Oskar <o.mizgier@gmail.com>
2019-11-19 16:46:24 +0000

    All auth functionality is working now

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({extended: true}));
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -29,15 +30,17 @@ app.get('/', function (req, res){
 	res.render('home')
 })
 
-app.get('/secret', function(req, res){
+app.get('/secret', isLoggedIn, function(req, res){
 	res.render('secret')
 })
 
 // AUTH ROUTES ==========================================
+
+//Handling user sign up
 app.get('/register', function (req, res){
 	res.render('register')
 })
-//Handling user sign up
+//Handle signup
 app.post('/register', function (req, res){
 	req.body.username
 	req.body.password
@@ -52,6 +55,34 @@ app.post('/register', function (req, res){
 		}
 	})
 })
+
+//Login routes
+app.get('/login', function (req, res){
+	res.render('login')
+})
+
+//Handle login
+//Middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+//LOGUT
+app.get('/logout', function (req, res){
+	req.logout();
+	res.redirect('/')
+})
+
+//Isloggedin
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/login')
+}
+
 
 //Tel express to listen for requests - start server
 app.listen(3000, function() { 

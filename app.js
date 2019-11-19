931b435a99ae3b168eaa5dec5a02585654a323c1
Oskar <o.mizgier@gmail.com>
2019-11-19 16:02:46 +0000

    Register functionality

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ app.use(require('express-session')({
 app.set('view engine', 'ejs')
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// ROUTES ---------------------------------------------
+// ROUTES =============================================
+
 app.get('/', function (req, res){
 	res.render('home')
 })
@@ -31,6 +33,25 @@ app.get('/secret', function(req, res){
 	res.render('secret')
 })
 
+// AUTH ROUTES ==========================================
+app.get('/register', function (req, res){
+	res.render('register')
+})
+//Handling user sign up
+app.post('/register', function (req, res){
+	req.body.username
+	req.body.password
+	User.register(new User({username: req.body.username}), req.body.password, function (err, user){
+		if(err){
+			console.log(err)
+			res.render('register')
+		} else {
+			passport.authenticate('local')(req, res, function(){
+				res.redirect('/secret')
+			})
+		}
+	})
+})
 
 //Tel express to listen for requests - start server
 app.listen(3000, function() { 
